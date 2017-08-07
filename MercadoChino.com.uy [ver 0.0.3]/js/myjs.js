765890d4ac07f4d64725d94126f59619bda67846
Juan-Guerra <studying.john@gmail.com>
2017-08-07 17:37:07 -0300

    Add files via upload
    MercadoChino.Uy [ver 0.0.3]

---
+++ MercadoChino.com.uy [ver 0.0.3]/js/myjs.js
@@ -0,0 +1,16 @@
+
+
+
+$(document).ready(function () {
+
+
+    $('.minav').load("./navbar.html");
+    $('.slider').slider({ interval: 8000, height: 900, indicators: false });
+    $('.parallax').parallax();
+    $('footer').load("./footer.html");
+
+});
+
+
+
+
